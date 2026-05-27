bff794f51941187a36d310d0eb97f2ab1bebbad8
Varghes125 <sibyfrancis12345@gmail.com>
2026-05-27 13:04:25 +0530

    Update cors.js

--- utils/cors.js
+++ utils/cors.js
@@ -1,8 +1,7 @@
 const ALLOWED_ORIGIN = "https://levelup-wine-one.vercel.app";
 
-export function setCors(res, req) {
+function setCors(res, req) {
   const origin = req.headers.origin;
-  // Allow the deployed frontend and localhost for dev
   if (origin === ALLOWED_ORIGIN || (origin && origin.startsWith("http://localhost"))) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   } else {
@@ -10,14 +9,15 @@ export function setCors(res, req) {
   }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
 }
 
-export function handleCors(req, res) {
+function handleCors(req, res) {
   setCors(res, req);
   if (req.method === "OPTIONS") {
     res.status(200).end();
-    return true; // signals caller to return early
+    return true;
   }
   return false;
 }
+
+module.exports = { handleCors };
